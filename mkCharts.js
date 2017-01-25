ca740b128390311f7531f9b917020daf496d719f
Matt Keppler <mkeppler@4iqsolutions.com>
2017-01-25 18:55:16 -0500

    Update mkCharts.js

--- mkCharts.js
+++ mkCharts.js
@@ -1,1 +1,34 @@
-var mkChartsModule = angular.module('mkCharts', []);
+var mkChartsModule = angular.module('mkCharts', [])
+.directive("mkChart", function() {
+  return {
+    restrict: 'E',
+    scope: {
+      
+      mkConfig: '=',
+      mkDataSource: '=',
+    },
+    template: function(elem, attr){
+      return '<mk-'+ attr.type +'></ mk-'+ attr.type +'>'; 
+    },
+    link: function(scope, elem, attr){
+      console.log('chart');
+    },
+    controller: function($scope){
+      $scope.type='mk-bar';
+      console.log($scope.mkDataSource, $scope.mkConfig, $scope)
+    }
+  };
+})
+.directive("mkBar", function() {
+  return {
+    restrict: 'AE',
+    require: '^^mkChart',
+    template: 'hi',
+    link: function(scope, elem, attr){
+      console.log('bar', scope.type, scope.mkDataSource, scope.mkConfig);
+    },
+    controller: function($scope){
+      //probably not needed
+    }
+  };
+});
