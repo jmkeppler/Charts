a92d9338fd89133a45aa8005a02f85ebccdddf1d
Matt Keppler <mkeppler@4iqsolutions.com>
2017-01-25 21:18:07 -0500

    Update mkCharts.js

--- mkCharts.js
+++ mkCharts.js
@@ -1,9 +1,42 @@
+/*var url ="https://www.gstatic.com/charts/loader.js";
+var JavaScript = {
+  load: function(src, callback) {
+    var existingScript = document.querySelector("script[src*='"+url+"']");
+    if (!existingScript) {
+      var script = document.createElement('script'),
+          loaded;
+      script.setAttribute('src', src);
+      if (callback) {
+        script.onreadystatechange = script.onload = function() {
+          if (!loaded) {
+            callback();
+          }
+          loaded = true;
+        };
+      }
+      document.getElementsByTagName('head')[0].appendChild(script);
+    }
+    else{
+      loaded = true;
+      callback();
+    }
+  }
+};
+
+//Load google graphs if it's not already there, then bootstrap the app.
+// The downside to this is that it forces any module that requires this to be 
+JavaScript.load(url, function() {
+  angular.element(function() {
+      angular.bootstrap(document, ['Test']);
+    });
+});
+*/
+
 var mkChartsModule = angular.module('mkCharts', [])
 .directive("mkChart", function() {
   return {
     restrict: 'E',
     scope: {
-      
       mkConfig: '=',
       mkDataSource: '=',
     },
@@ -23,9 +56,34 @@ var mkChartsModule = angular.module('mkCharts', [])
   return {
     restrict: 'AE',
     require: '^^mkChart',
-    template: 'hi',
+    template: '<div></div>',
     link: function(scope, elem, attr){
       console.log('bar', scope.type, scope.mkDataSource, scope.mkConfig);
+      google.charts.load('current', {packages: ['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+      
+      
+      function drawChart(){
+          // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1], 
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':500,
+                       'height':300};
+
+        var chart = new google.visualization.BarChart(elem[0]);
+        chart.draw(data, options);
+      }
     },
     controller: function($scope){
       //probably not needed
